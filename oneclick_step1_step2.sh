d340a56eb9a81866956f4a22eacd4ddf6e2f28c7
heavytail <k0s1k0s1k0@korea.ac.kr>
2024-05-04 17:13:51 +0900

    minor fix

--- oneclick_step1_step2.sh
+++ oneclick_step1_step2.sh
@@ -3,8 +3,8 @@
 
 # AVAILABLE MODEL TYPE: kullm3, gemma, solar, mistral, openai, koalpaca_v1_1b
 
-SAVE_NAME=ko-gemma-1_1-v2-lstsq_all_second_step240504
-MODEL_PATH="/your/model/path"
+SAVE_NAME=your_save_name
+MODEL_PATH=/your/model/path
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 step1_generate_answers.py \
 --model_name $MODEL_PATH \
